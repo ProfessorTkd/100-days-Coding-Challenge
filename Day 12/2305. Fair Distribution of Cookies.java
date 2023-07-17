84d813d16f0fe6b71dceb016370f9f469cc75941
Tapish Rawat <iamtapishrawat@gmail.com>
2023-07-17 11:28:28 +0530

    Create 2305. Fair Distribution of Cookies.java

---
+++ Day 12/2305. Fair Distribution of Cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    int res = Integer.MAX_VALUE;
+    
+    public int distributeCookies(int[] cookies, int k) {
+        dfs(cookies, new int [k], 0, 0);
+        return res;
+    }
+    
+    public void dfs(int [] cookies, int [] children, int idx, int max) {
+        if (idx >= cookies.length) {
+            res = Math.min(res, max);
+            return;
+        }
+        
+        int c = cookies[idx];
+        for (int i = 0; i < children.length; i++) {
+            children[i] += c;
+            dfs(cookies, children, idx + 1, Math.max(max, children[i]));
+            children[i] -= c;
+            if(children[i]==0) return; // each child must get atleast one cookie
+        }
+    }
+}
